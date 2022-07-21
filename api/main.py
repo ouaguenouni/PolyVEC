81c4411a0aa119c8617885e3afa9f30e3dac3c20
steagsinc <steags.inc@gmail.com>
2022-07-21 17:01:44 +0200

    api

--- api/main.py
+++ api/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+import random
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/randPos")
+async def randPos():
+    return {"message": random.randrange(0,577)}
